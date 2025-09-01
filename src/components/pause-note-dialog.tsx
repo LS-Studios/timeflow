8b0e0b54e12cb050e1652f983c40ff9baf7f5f4c
Lennard Stubbe <stubbelennard135@gmail.com>
2025-09-01 08:23:49 +0000

    immmer noch der dialog hat keine korrekten padding, sodnern nur links ak

--- src/components/pause-note-dialog.tsx
+++ src/components/pause-note-dialog.tsx
@@ -59,7 +59,7 @@ export function PauseNoteDialog({
 
   return (
     <Dialog open={isOpen} onOpenChange={handleOpenChange}>
-      <DialogContent className="sm:max-w-[425px] mx-4">
+      <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>{t('addPauseNote')}</DialogTitle>
           <DialogDescription>
